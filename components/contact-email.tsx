386137ed26860d1b6bae4faed6b09d6dc45bd92b
Deepak Chhantyal <deepakchhantyal4156@gmail.com>
2026-02-06 13:59:49 +0545

    project iniliazation

---
+++ components/contact-email.tsx
@@ -0,0 +1,8 @@
+export default function ContactEmail() {
+  return (
+    <a href="mailto:huzaifa2008khi@gmail.com" className="text-purple-400 hover:text-purple-300 mt-2 inline-block">
+      huzaifa2008khi@gmail.com
+    </a>
+  )
+}
+
